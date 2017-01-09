043ec7b5db3b13d677ef9f46eda544710406641e
Joshua Tauberer <jt@occams.info>
2017-01-09 12:08:00 -0500

    move au_80053_audit_set1.yaml into a new private-assets subdirectory that Q will now ignore so that the IssuePacks YAML file isn't loaded as if it were a Q module YAML file
    see https://github.com/GovReady/govready-q/issues/224

--- modules/issuepack.py
+++ modules/issuepack.py
@@ -58,7 +58,7 @@ def create_issue_pack(q, answers):
             '-p=' + answers['jira_password'],
             '-k=' + answers['jira_project_key'],
             '-b=' + answers['jira_project_base_uri'],
-            os.path.join(os.path.dirname(__file__), 'au_80053_audit_set1.yaml'),
+            os.path.join(os.path.dirname(__file__), 'private-assets', 'au_80053_audit_set1.yaml'),
         ]).decode("utf8")
         status = "success"
     except Exception as e:
