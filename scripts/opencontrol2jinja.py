880280a59eec841d9e034a7e564fbbc2e131d9e9
Joshua Tauberer <jt@occams.info>
2017-07-07 15:39:23 -0400

    drupal website: add a second ssp output module that uses the FISMA-style SSP template and update the script to generate this sort of output

--- scripts/opencontrol2jinja.py
+++ scripts/opencontrol2jinja.py
@@ -49,16 +49,95 @@ for control_key, control_text in controls_sorted:
     control_key_str = re.sub("[^a-z0-9]", "_", control_key.lower()).strip("_")
 
     template_text += """
-    <h2>{}</h2>""".format(control_key)
+      <h3>{control_key}</h3>
+      <table class="table" style="margin-top: 1.5em">
+        <thead>
+          <th colspan="2">
+            <div class="row">
+              <div class="col-xs-3">
+                {control_key}
+              </div>
+              <div class="col-xs-9">
+                Control Summary Information
+              </div>
+            </div>
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td width="30%">Responsible Role:</th>
+            <td>
+                ...
+            </td>
+          </tr>
+          <tr>
+            <td>Parameter:</th>
+            <td>
+                ...
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div style="margin-bottom: .5em">Implementation Status:</div>
+              +% for key, text in [
+      ["implemented", "Implemented"],
+      ["partially_implemented", "Partially Implemented"],
+      ["planned", "Planned"],
+      ["alternative_implementation", "Alternative Implementation"],
+      ["not_applicable", "Not Applicable"]] %~
+              <div class="radio">
+                <label>
+                  <input type="checkbox" disabled="disabled"> ++text~~
+                </label>
+              </div>
+              +% endfor %~
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div style="margin-bottom: .5em">Control Origination:</div>
+              +% for key, text in [["service_provider_corporate", "Service Provider Corporate"],
+      ["service_provider_system_specific", "Service Provider System Specific"],
+      ["service_provider_hybrid", "Service Provider Hybrid (Corporate and System Specific)"],
+      ["configured_by_customer", "Configured by Customer (Customer System Specific)"],
+      ["provided_by_customer", "Provided by Customer (Customer System Specific)"],
+      ["shared", "Shared (Service Provider and Customer Responsibility)"],
+      ["inherited", "Inherited from enterprise IT service"]] %~
+              <div class="radio">
+                <label>
+                  <input type="checkbox" disabled="disabled"
+                    > ++text~~
+                </label>
+              </div>
+              +% endfor %~
+            </td>
+          </tr>
+        </body>
+      </table>
+      <table class="table">
+        <thead>
+          <th colspan="2">
+            {control_key} What is the solution and how is it implemented?
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td colspan="2">
+                +% if not project.infrastructure %~
+                  There will be infrastructure.
+                +% else %~
+                  ++project.infrastructure.output_documents.ssp_template_{control_key_str}|safe~~
+                +% endif %~
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      """.format(
+        control_key=control_key,
+        control_key_str=control_key_str,
+    ).replace("+","{").replace("~","}")
 
-    template_text += """
-    +% if not project.infrastructure %~
-      There will be infrastructure.
-    +% else %~
-      ++project.infrastructure.output_documents.ssp_template_{}|safe~~
-    +% endif %~\n""".format(control_key_str).replace("+","{").replace("~","}")
-
-    template_text += "{}".format(control_text)
+    #template_text += "{}".format(control_text)
 
     # for doc in outputs:
     #     if doc.get("id") == "ssp_template_" + control_key:
