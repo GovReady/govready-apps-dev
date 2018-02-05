7db65c8c77702c90ac19a59e23c74c43310843ef
Joshua Tauberer <jt@occams.info>
2018-02-04 19:45:52 -0500

    update FISMA Level app
    Fix prompts (this was developed when question titles were displayed in the prompt), improve some text, improve automatically generated questions. Update the script that automatically generates the information type questions. Remove an obsolete script.

--- scripts/fisma_level_information_types.py
+++ scripts/fisma_level_information_types.py
@@ -1,3 +1,8 @@
+# This script updates the fisma_level app's fisma_level module,
+# which walks the user through choosing information types and
+# then confirming the high water mark level for all of the selected
+# types, across confidentiality, integrity, and availability.
+
 from collections import OrderedDict
 import rtyaml
 import re
@@ -6,16 +11,21 @@ infotypes = rtyaml.load(open("information_types.yaml"))
 
 all_info_types = []
 seen_infotype_parent_q = set()
-infotype_questions = OrderedDict([
-	("information_types_categories", OrderedDict([
-		("id", "information_types_categories"),
-		("title", "Which categories of information does your system contain?"),
-		("prompt", "Check all that apply."),
-		("type", "multiple-choice"),
-		("choices", []),
-	])),
-])
+infotype_questions = OrderedDict()
 
+# Create the root question that kicks off which information categories and types
+# the user is asked about.
+infotype_questions["information_types_categories"] = OrderedDict([
+	("id", "information_types_categories"),
+	("title", "Information Types Categories"),
+	("prompt", "Which categories of information does your system contain?\n\nCheck all that apply.\n"),
+	("type", "multiple-choice"),
+	("choices", []),
+	])
+
+# Recursively process the raw information types data tracing a path to
+# the actual information types (i.e. leaf nodes in the tree), through the
+# information type categories.
 def process_information_types(infotypes, path=[]):
 	for subtype in infotypes:
 		if "subtypes" not in subtype:
@@ -39,8 +49,8 @@ def process_information_types(infotypes, path=[]):
 			infotype_questions.setdefault(p_key,
 				OrderedDict([
 					("id", p_key),
-					("title", "Which types of %s information does your system contain?" % parent_q["title"]),
-					("prompt", "Check all that apply."),
+					("title", parent_q["title"] + " Information Types"),
+					("prompt", "Which types of %s information does your system contain?\n\nCheck all that apply.\n" % parent_q["title"]),
 					("type", "multiple-choice"),
 					("choices", []),
 					("impute", [
@@ -53,7 +63,7 @@ def process_information_types(infotypes, path=[]):
 					OrderedDict([
 						("key", subtype["code"]),
 						("text", subtype["title"]),
-						("help", subtype["description"]["text"].strip() + " (NIST 800-60 Vol 2 " + subtype["code"] + ")\n"),
+						("help", subtype["description"]["text"].strip() + " (NIST SP 800-60 Vol II Rev 1 " + subtype["code"] + ")" + ("\n" if "\n" in subtype["description"]["text"].strip() else "")), # YAML is prettier if multi-line strings end with a newline
 					])			
 			)
 
@@ -67,11 +77,11 @@ def process_information_types(infotypes, path=[]):
 # Recurse.
 process_information_types(infotypes)
 
-# Add a final question that collects all of the infotypes selected using an impute condition.
+# Add a final always-imputed question that collects all of the infotypes selected.
 infotype_questions_actual = [q for q in infotype_questions.values() if q["id"] != "information_types_categories"]
 infotype_questions["information_types"] = OrderedDict([
 	("id", "information_types"),
-	("title", "Which information types does your system contain?"),
+	("title", "Information types contained in the system"),
 	("prompt", "[not used, always imputed]"),
 	("type", "multiple-choice"),
 	("choices", sum([[{ "key": c["key"], "text": c["text"] } for c in q["choices"]] for q in infotype_questions_actual], [])),
@@ -106,7 +116,7 @@ for impact_type in ("confidentiality", "integrity", "availability"):
 		return grouper(infotypes, 25)
 	infotype_questions["information_types_%s_provisional" % impact_type] = OrderedDict([
 		("id", "information_types_%s_provisional" % impact_type),
-		("title", "What is the provisional %s impact level?" % impact_type),
+		("title", "Provisional %s impact level" % impact_type),
 		("prompt", "[not used, always imputed]"),
 		("type", "text"),
 		("impute",
@@ -127,7 +137,7 @@ for impact_type in ("confidentiality", "integrity", "availability"):
 	])
 
 # Update the module file in place.
-module = rtyaml.load(open("../modules/fisma_level.yaml"))
+module = rtyaml.load(open("../apps/fisma_level/fisma_level.yaml"))
 
 # Add/replace the information type questions.
 for q in infotype_questions.values():
@@ -141,7 +151,7 @@ for q in infotype_questions.values():
 
 # For each of confidentiality, integrity, and availability, we have to ask if
 # the user wants to downgrade or upgrade from (the high water mark across their
-# information types of) the provisional categorization given by NIST 800-60 vol2.
+# information types of) the provisional categorization given by NIST SP 800-60 Vol II.
 # We insert guidance from the information types the user selected.
 
 for impact_type in ("confidentiality", "integrity", "availability"):
@@ -174,6 +184,6 @@ The provisional {impact_type} impact level of {title} information is **{level}**
 		q["prompt"])
 
 # Save the module back out.
-with open("../modules/fisma_level.yaml", "w") as f:
+with open("../apps/fisma_level/fisma_level.yaml", "w") as f:
 	f.write(rtyaml.dump(module))
 
